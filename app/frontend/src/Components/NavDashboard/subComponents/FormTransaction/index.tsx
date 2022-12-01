71c832f3098e2e3f0445515b2a74635379bfc901
erik-efl <erik.lima.f@outlook.com>
2022-12-01 00:24:33 -0300

    feat: :sparkles: Transactions components

---
+++ app/frontend/src/Components/NavDashboard/subComponents/FormTransaction/index.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+import Request from '../../../../services/api'
+import { CreateTransaction } from '../../../../types/types'
+
+
+function FormTransaction() {
+  const queryClient = useQueryClient()
+  const [transaction, setTransaction] = useState<CreateTransaction>({
+    nameDebit: '',
+    nameCredited: '',
+    value: '',
+  })
+  const [success, setSuccess] = useState('')
+  const [resError, setResError] = useState('')
+
+  const { data, isError, isLoading, error } = useQuery('user', () => Request.getUserInfo())
+
+  const { mutate: addNewTransaction } = useMutation((newTransaction: object | string) => {
+    return Request.createTransaction(newTransaction)
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users'),
+      queryClient.invalidateQueries('amount'),
+      setSuccess('Transação realizada com sucesso')
+    }
+  })
+
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <>Error: {error}</>
+
+  const { user } = data?.data
+
+  const handleSubmit = async (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    addNewTransaction(transaction)
+    setTransaction({
+      nameDebit: '',
+      nameCredited: '',
+      value: '',
+    })
+  }
+
+  return (
+    <div>
+      <input
+        type="number"
+        name="trValue"
+        id="trValue"
+        value={transaction.value}
+        onChange={(event) => setTransaction({ ...transaction, value: event.target.value })}
+        placeholder='Insira um valor' />
+      <input
+        type="text"
+        name="trName"
+        id="trName"
+        value={transaction.nameCredited}
+        onChange={(event) => setTransaction({ ...transaction, nameCredited: event.target.value, nameDebit: user.username })}
+        placeholder='Para quem?' />
+      <button
+        className='transfer'
+        type='submit'
+        onClick={handleSubmit}
+      >
+        Transferir
+      </button>
+    </div>
+  )
+}
+
+export default FormTransaction
