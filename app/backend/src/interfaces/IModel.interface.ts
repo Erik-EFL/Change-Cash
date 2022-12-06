9240732341980b179e92d0fd5950a354e254fe40
erik-efl <erik.lima.f@outlook.com>
2022-12-05 23:48:20 -0300

    chore: :fire: Remove files

--- app/backend/src/interfaces/IModel.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IModel<T> {
-  create(obj:T):Promise<T>,
-  read():Promise<T[]>,
-  readOne(id:string):Promise<T | null>,
-  update(id:string, obj:T):Promise<T | null>,
-  delete(id:string):Promise<T | null>,
-}
