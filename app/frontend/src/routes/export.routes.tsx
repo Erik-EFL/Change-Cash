5d259fe207bb983e21f8065e47313fabec4b4ada
erik-efl <erik.lima.f@outlook.com>
2022-12-12 19:24:14 -0300

    fix: :sparkles: Change routes name

--- app/frontend/src/routes/export.routes.tsx
+++ app/frontend/src/routes/export.routes.tsx
@@ -1,18 +1,37 @@
+import { useEffect, useState } from "react";
 import {  BrowserRouter,  Routes,  Route,} from "react-router-dom";
-import Home from '../pages/Home/index';
-import Dashboard from '../pages/dashBoard';
+import Home from '../pages/transactions/index';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 
 function Routing() {
+  const [isLogued, setIsLogued] = useState(true)
+
+  const handleLogin = () => {
+    const user = localStorage.getItem('user')
+    if (!user) {
+      setIsLogued(false)
+    } else {
+      setIsLogued(true)
+    }
+  }
+
+  useEffect(() => {
+    handleLogin()
+  }, [isLogued])
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register  />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Home />} />
+          {
+            isLogued
+              ? <Route path="/dashboard" element={<Home />} />
+              : <Route path="/" element={<Login />} />
+          }
         </Routes>
       </BrowserRouter>
     </>
