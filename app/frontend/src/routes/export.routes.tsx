bae68226dac376a836c507e2a5687969632f5dfc
erik-efl <erik.lima.f@outlook.com>
2022-12-20 21:13:21 -0300

    feat: :sparkles: switching routing format
    Use createBrouserRouter
    
    this way makes it more practical to build routing and add middleware to the front end of the project

--- app/frontend/src/routes/export.routes.tsx
+++ app/frontend/src/routes/export.routes.tsx
@@ -1,41 +1,26 @@
 import { useEffect, useState } from "react";
-import {  BrowserRouter,  Routes,  Route,} from "react-router-dom";
+import {  createBrowserRouter } from "react-router-dom";
 import Home from '../pages/transactions/index';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import ErrorPage from "../error/pages/ErrorPage";
 
-function Routing() {
-  const [isLogued, setIsLogued] = useState(true)
-
-  const handleLogin = () => {
-    const user = localStorage.getItem('user')
-    if (!user) {
-      setIsLogued(false)
-    } else {
-      setIsLogued(true)
-    }
+const Routing = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/dashboard",
+    element: <Home/>,
+    errorElement: <ErrorPage />,
   }
-
-  useEffect(() => {
-    handleLogin()
-  }, [isLogued])
-
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register  />} />
-          <Route path="/dashboard" element={<Home />} />
-          {
-            isLogued
-              ? <Route path="/dashboard" element={<Home />} />
-              : <Route path="/" element={<Login />} />
-          }
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
-}
+])
 
 export default Routing
